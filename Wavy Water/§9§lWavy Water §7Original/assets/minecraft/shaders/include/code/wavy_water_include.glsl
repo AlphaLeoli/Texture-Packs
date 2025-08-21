ab544117baf3c8bea5b577d1f00c3dc4c23dd588
AlphaLeoli <thealphaleoli@gmail.com>
2025-08-21 00:14:37 -0400

    recoded the entire pack, added a framework for easier updating in the future, temporarily removed respackopts support, created a new icon, and ported the pack to all versions 1.20.5 to 1.21.8

---
+++ Wavy Water/§9§lWavy Water §7Original/assets/minecraft/shaders/include/code/wavy_water_include.glsl
@@ -0,0 +1,18 @@
+#version 150
+
+float time = GameTime * speed;
+
+// I might have made this a lot more complicated with sum of sines and
+// fractal brownian motion and things like that, but i think the simplicity
+// fits into minecraft a bit more. In the future, I may make a "realistic water"
+// texture pack that uses all those + more, but for now it will remain simple.
+float getWaveOffset() {
+    if (round(texture(Sampler0, UV0).a * 1000.0) != 706.0) return 0.0;
+    float x = abs(Position.x - 8.0);
+    float z = abs(Position.z - 8.0);
+
+    float xWave = 1.1 * height * sin((round(x * frequency) + time * 0.9));
+    float zWave = 0.9 * height * cos((round(z * frequency) + time * 1.1));
+
+    return (xWave + zWave) * 0.25;
+}
