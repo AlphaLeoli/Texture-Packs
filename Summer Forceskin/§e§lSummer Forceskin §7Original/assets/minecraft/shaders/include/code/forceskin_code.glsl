9f398d2bc9043f365a7c9c30fb18a483b373d1e7
AlphaLeoli <thealphaleoli@gmail.com>
2025-08-28 05:52:28 -0400

    initial creation of the forceskin pack

---
+++ Summer Forceskin/§e§lSummer Forceskin §7Original/assets/minecraft/shaders/include/code/forceskin_code.glsl
@@ -0,0 +1,16 @@
+#version 150
+
+bool isPlayer = textureSize(Sampler0, 0) == ivec2(64) &&
+                texture(Sampler0, vec2(0.0078125)).a == 1.0;
+
+vec4 getForceskinColor() {
+    if (!isPlayer) return texture(Sampler0, texCoord0);
+
+    int id = int(texCoord0.x * 64.0) * 64 +
+             int(texCoord0.y * 64.0);
+
+    ivec4 forceskinColor = getSkinColor(id);
+    if (forceskinColor.a == 0) return texture(Sampler0, texCoord0);
+    if (all(lessThan(forceskinColor - ivec4(189, 89, 221, 89), vec4(1e-3)))) return vec4(0.0);
+    return forceskinColor * (1.0 / 255.0);
+}
