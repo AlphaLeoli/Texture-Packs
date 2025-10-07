534bf52a7bc30aaa0c5c2f1f4f6644d60c8920c5
AlphaLeoli <thealphaleoli@gmail.com>
2025-10-07 12:57:20 -0400

    updated to 1.21.9

--- Wavy Water/assets/minecraft/shaders/include/config.glsl
+++ Wavy Water/assets/minecraft/shaders/include/config.glsl
@@ -1,4 +1,4 @@
-#version 150
+#version 330
 
 const float height = 0.25;   // 0.1 to 0.5 works best
 const float frequency = 0.5; // I think the default is the best, but you can play around with it more
