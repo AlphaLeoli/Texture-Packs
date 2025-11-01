2f7b323b5b65ea3ff870e9a391ba47239f0b1495
AlphaLeoli <thealphaleoli@gmail.com>
2025-11-01 00:15:36 -0400

    added respackopts support, fixed fatal typo

--- Wavy Water/assets/minecraft/shaders/include/code/wavy_water_include.glsl
+++ Wavy Water/assets/minecraft/shaders/include/code/wavy_water_include.glsl
@@ -2,10 +2,6 @@
 
 float time = GameTime * speed;
 
-// I might have made this a lot more complicated with sum of sines and
-// fractal brownian motion and things like that, but i think the simplicity
-// fits into minecraft a bit more. In the future, I may make a "realistic water"
-// texture pack that uses all those + more, but for now it will remain simple.
 float getWaveOffset() {
     if (round(texture(Sampler0, UV0).a * 1000.0) != 706.0) return 0.0;
     float x = abs(Position.x - 8.0);
--- Wavy Water/assets/minecraft/shaders/include/config.glsl
+++ Wavy Water/assets/minecraft/shaders/include/config.glsl
@@ -1,5 +1,3 @@
-#version 330
-
 const float height = 0.25;   // 0.1 to 0.5 works best
 const float frequency = 0.5; // I think the default is the best, but you can play around with it more
 const float speed = 3000.0;  // 500 - 5000 works best
