57286bda6988314df3cd76704f05fd1242d7e0c0
AlphaLeoli <thealphaleoli@gmail.com>
2026-08-24 01:34:23 -0400

    added respackopts support, updated to versions 1.21.11-26.2

---
+++ Windy Leaves/assets/minecraft/shaders/include/code/wavy_leaves_respackopts.glsl
@@ -0,0 +1,4 @@
+// Don't edit this file directly, please use respackopts or edit config.glsl if you don't have it.
+const float height = ${height};
+const float frequency = ${frequency};
+const float speed = ${speed};
